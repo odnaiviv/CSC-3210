f4442047bf5e726a230d0c3047fe42af3bd149fb
Vivian Do <odnaiviv@hotmail.com>
2021-11-14 19:36:20 -0500

    Update lab7a.asm

--- Labs/Lab 07/lab7a.asm
+++ Labs/Lab 07/lab7a.asm
@@ -15,9 +15,9 @@ main1 proc
 	mov ax, wVal
 	mov count, al
 
-	mov ax, wVal			; changed al to ax													(originally was mov al, wVal)
-	mov al, count			; changed ax to al													(originall was mov ax, count)
-	mov al, count			; changed eax to al													(originall was mov eax, count)
+	mov ax, wVal			; changed al to ax							(originally was mov al, wVal)
+	mov al, count			; changed ax to al							(originall was mov ax, count)
+	mov al, count			; changed eax to al							(originall was mov eax, count)
 	mov ax, wVal2			; changed switched wVal with wVal2 and then moved wVal2 to ax		(originally was mov wVal2, wVal)
 	mov ax, wVal			; added another instruction to mov wVal to ax
 	invoke ExitProcess, 0
