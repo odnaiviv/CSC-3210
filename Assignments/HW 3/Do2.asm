5685dea6ef5c7c76abffebf68c43521619d10eaa
Vivian Do <odnaiviv@hotmail.com>
2021-11-16 21:36:54 -0500

    Update Do2.asm

--- Assignments/HW 3/Do2.asm
+++ Assignments/HW 3/Do2.asm
@@ -9,28 +9,28 @@
 ExitProcess proto, dwExitCode:dword
 
 .data
-	inputStr BYTE "ABCDEFGH"					; creating given array as BYTE, but all in one string
+	inputStr BYTE "ABCDEFGH"				; creating given array as BYTE, but all in one string
 
 .code
 main2 proc
-	mov esi, 0									; putting zeros in the register value
+	mov esi, 0						; putting zeros in the register value
 	mov edi, 0
 
-	mov esi, OFFSET inputStr					; moving beginning of array to esi
-	mov edi, esi								; moving end of array to edi
+	mov esi, OFFSET inputStr				; moving beginning of array to esi
+	mov edi, esi						; moving end of array to edi
 	add edi, SIZEOF inputStr
 
 	mov ecx, LENGTHOF inputStr / 2				; setting counter for loop
 
-	L1:											; loop
-		mov al, [esi]							; putting first and last element in eax and ebx register respectively
+	L1:							; loop
+		mov al, [esi]					; putting first and last element in eax and ebx register respectively
 		mov bl, [edi]
 
-		xchg bl, al								; swapping elements and putting elements back into array
+		xchg bl, al					; swapping elements and putting elements back into array
 		mov [edi], bl
 		mov [esi], al
 
-		add esi, TYPE inputStr					; switching target element
+		add esi, TYPE inputStr				; switching target element
 		sub edi, TYPE inputStr
 
 		loop L1
