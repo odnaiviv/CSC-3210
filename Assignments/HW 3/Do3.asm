9c4d79c27fc02b07fd82f2099c9348496855d4b6
Vivian Do <odnaiviv@hotmail.com>
2021-11-16 21:37:37 -0500

    Update Do3.asm

--- Assignments/HW 3/Do3.asm
+++ Assignments/HW 3/Do3.asm
@@ -14,19 +14,19 @@ ExitProcess proto, dwExitCode:dword
 
 .code
 main3 proc
-	mov eax, 0							; putting zeros in the register values
+	mov eax, 0						; putting zeros in the register values
 	mov ebx, 0
 	mov ecx, 0
 	mov edx, 0
 
-	mov ax, WORD PTR [qVal + 6]			; loading 0506h in ax
-	mov bx, WORD PTR [qVal + 4]			; loading 0307h in bx
-	mov cx, WORD PTR [qVal + 2]			; loading 4080h in cx
-	mov dx, WORD PTR [qVal]				; loading 0102h in dx
+	mov ax, WORD PTR [qVal + 6]				; loading 0506h in ax
+	mov bx, WORD PTR [qVal + 4]				; loading 0307h in bx
+	mov cx, WORD PTR [qVal + 2]				; loading 4080h in cx
+	mov dx, WORD PTR [qVal]					; loading 0102h in dx
 
-	add ax, bx							; adding up the values of each register into ax
+	add ax, bx						; adding up the values of each register into ax
 	add ax, cx
-	add ax, dx							; EAX = 00000D17h
+	add ax, dx						; EAX = 00000D17h
 
 	invoke ExitProcess, 0
 
