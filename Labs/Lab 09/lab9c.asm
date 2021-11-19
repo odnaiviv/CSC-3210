420d42470eac5da4e010d4c3c803e0e48a41268d
Vivian Do <odnaiviv@hotmail.com>
2021-11-19 01:43:42 -0500

    Update lab9c.asm

--- Labs/Lab 09/lab9c.asm
+++ Labs/Lab 09/lab9c.asm
@@ -7,13 +7,13 @@ ExitProcess proto, dwExitCode:dword
 .code
 main proc
 	mov al, 01101111b
-	and al, 00101101b			; a. AL = 
+	and al, 00101101b			; a. AL = 2Dh
 	mov al, 6Dh
-	and al, 4Ah					; b. AL = 
+	and al, 4Ah				; b. AL = 48h
 	mov al, 00001111b
-	or al, 61h					; c. AL = 
+	or al, 61h				; c. AL = 6Fh
 	mov al, 94h
-	xor al, 37h					; d. AL = 
+	xor al, 37h				; d. AL = A3h
 	invoke ExitProcess, 0
 
 main endp
