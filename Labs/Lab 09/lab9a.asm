e9fe2b2f10dd9e56fc999168431e27cd015b268c
Vivian Do <odnaiviv@hotmail.com>
2021-11-19 01:39:02 -0500

    Update lab9a.asm

--- Labs/Lab 09/lab9a.asm
+++ Labs/Lab 09/lab9a.asm
@@ -11,11 +11,11 @@ ExitProcess proto, dwExitCode:dword
 main1 proc
 	mov edi, OFFSET intarray			; EDI = address of intarray
 	mov ecx, LENGTHOF intarray			; initialize loop counter
-	mov eax, 0							; sum = 0
-	L1:									; mark beginning of loop
-	add eax, [edi]						; add an integer
+	mov eax, 0					; sum = 0
+	L1:						; mark beginning of loop
+	add eax, [edi]					; add an integer
 	add edi, TYPE intarray				; point to next element
-	loop L1								; repeat until EXC = 0
+	loop L1						; repeat until EXC = 0
 	invoke ExitProcess, 0
 
 main1 endp
