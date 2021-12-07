30410f2d287ecda5e523a3be483c753fd8c0d9ee
Vivian Do <odnaiviv@hotmail.com>
2021-12-07 14:53:11 -0500

    Add files via upload

---
+++ Assignments/HW 4/Do1.asm
@@ -0,0 +1,88 @@
+; Vivian Do
+; Class: CSC 3210
+; Assignment 4 Question 1
+; This program will convert the given pseudocode into x86 assembly
+; The given pseudocode will be written at the bottom of my code
+
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess proto, dwExitCode:dword
+
+.data
+	sum WORD 0
+	i WORD 0
+	j WORD 12
+	var1 WORD 3
+	var2 WORD 3
+	var3 WORD 0
+
+.code
+main1 proc
+	mov eax, 0						; putting zeros in the register values
+	mov ebx, 0
+	mov ecx, 0
+	mov edx, 0
+
+	mov ax, i						; putting i into eax register
+	mov bx, j						; putting j into ebx register
+	mov cx, var1					; putting var1 into ecx register
+	mov dx, var2					; putting var2 into edx register
+
+	while_block:					; start of while loop
+		cmp ax, 20
+		jge endwhile_block			; jumping to end if i >= 20
+		cmp bx, -2
+		jle endwhile_block			; jumping to end if j <= -2
+		jmp if_block
+
+	if_block:						; if statement
+		cmp cx, var3				; comparing var1 with var3
+		jle elseif_block
+		sub cx, ax					; var1 - i
+		jmp continue
+
+	elseif_block:					; else if statement
+		cmp dx, var3				; comparing var2 with var3
+		jge else_block
+		add dx, bx					; var2 + j
+		jmp continue
+
+	else_block:						; else statement
+		add var3, ax				; var3 + i
+		jmp continue
+		
+	continue: 
+		mov di, cx					; adding up the vars in edi register
+		add di, dx
+		add di, var3
+		add ax, 1					; i + 1 or incrementing i
+		sub bx, 1					; j - 1 or decrementing j
+		jmp while_block				; looping back to beginning of while loop
+		
+	endwhile_block:					; putting edi register into sum
+		mov sum, di
+	
+	invoke Exitprocess, 0
+main1 endp
+end main1
+
+; given pseudocode from question 1
+;
+; sum = 0
+; i = 0
+; j = 12
+; var1 = 3
+; var2 = 3
+; var3 = 0
+; while ((i < 20) and (j > -2)) {
+;	if (var1 > var3): 
+;		var1 = var1 - i
+;	else if (var2 < var3): 
+;		var2 = var2 + j
+;	else: 
+;	var3 = var3 + i
+; sum = var1 + var2 + var3
+; i = i + 1
+; j = j - 1
+; }
---
+++ Assignments/HW 4/Do2.asm
@@ -0,0 +1,26 @@
+; Vivian Do
+; Class: CSC 3210
+; Assignment 4 Question 2
+; This program will test if MSB in al register is set
+; If set, al is divided by 8 using shift and exiting; if not set, al is multiplied by 4 using shift and exiting
+
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess proto, dwExitCode:dword
+
+.code
+main2 proc
+	mov eax, 0						; putting zeros in the register values
+	mov al, 88h
+
+	test al, 1000000b				; bit masking to check MSB
+	jnz msb_set						; msb is set
+	sal al, 2						; msb is not set
+
+	msb_set: 
+		sar al, 3
+
+	invoke Exitprocess, 0			; End Results: AL = F1
+main2 endp
+end main2
